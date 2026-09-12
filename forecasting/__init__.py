e15d5c6ec6d36efe7ec40b9a67ad335c2a0bd511
yaksh vachhani <yakshvachhani1@gmail.com>
2026-09-12 15:09:37 +0530

    Delete forecasting directory
    reconstruct forcasting.

--- forecasting/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Forecasting package for renewable generation (solar/wind) and load demand."""
-
-try:
-    from forecasting.predict import forecast
-    __all__ = ["forecast"]
-except ImportError:
-    pass
